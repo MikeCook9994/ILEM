912015a5203f1163cf3ceb8a91aa3680a70688af
MikeCook9994 <mjcook3@wisc.edu>
2017-08-30 08:23:59 -0700

    added i2c communication C# code and arduino sketch.

--- Arduino/sketches/i2cTest/i2ctest.ino
+++ Arduino/sketches/i2cTest/i2ctest.ino
@@ -1,14 +1,28 @@
 #include <Wire.h>  
 
-#define SLAVE_ADDRESS 0x40   // Define the i2c address
-
-void setup()
-{
-    Serial.begin(9600);
-    Wire.begin(SLAVE_ADDRESS); 
+void setup() {
+  Wire.begin(0x40);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  Serial.begin(9600);           // start serial for output
 }
 
-void loop()
-{
-    
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  Wire.write("Hello Raspberry pi!");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (Wire.available() > 0) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  Serial.println("");
 }
--- ILEM/ILEM/MainPage.xaml.cs
+++ ILEM/ILEM/MainPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using Windows.UI.Xaml.Controls;
 using Windows.Devices.Enumeration;
 using Windows.Devices.I2c;
-using System.Diagnostics;
-using System.Threading;
 using System;
 using Windows.UI.Xaml;
 
@@ -15,32 +13,57 @@ namespace ILEM
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private I2cDevice Device;
-        private Timer periodicTimer;
+        private I2cDevice arduino;
+        private DispatcherTimer timer;
+
         public MainPage()
         {
             this.InitializeComponent();
-            //InitializeSerialCommunication();
+            InitializeSerialCommunication();
         }
 
         private async void InitializeSerialCommunication()
         {
             var settings = new I2cConnectionSettings(0x40); // Arduino address
-            settings.BusSpeed = I2cBusSpeed.StandardMode;
+
             string aqs = I2cDevice.GetDeviceSelector("I2C1");
             var dis = await DeviceInformation.FindAllAsync(aqs);
-            Device = await I2cDevice.FromIdAsync(dis[0].Id, settings);
-            periodicTimer = new Timer(this.TimerCallback, null, 0, 100); // Create a timmer
-        }
+            arduino = await I2cDevice.FromIdAsync(dis[0].Id, settings);
 
-        private void TimerCallback(object state)
-        {
+            Messages.Text += $"{dis.Count} --- ";
+            for(int i = 0; i < dis.Count; i++)
+            {
+                Messages.Text += $"{dis[i].Name} --- ";
+            }
 
+            Messages.Text += arduino.DeviceId;
+            if (arduino == null)
+            {
+                Messages.Text = string.Format(
+                    "Slave address {0} on I2C Controller {1} is currently in use by " +
+                    "another application. Please ensure that no other applications are using I2C.",
+                    settings.SlaveAddress,
+                    dis[0].Id);
+            }
+
+            this.timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick; // We will create an event handler 
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000); // Timer_Tick is executed every 500 milli second
+            timer.Start();
         }
 
         private void ClickMe_Click(object sender, RoutedEventArgs e)
         {
             this.HelloMessage.Text = "Hello, Windows 10 IoT Core!";
+            this.arduino.Write(System.Text.Encoding.ASCII.GetBytes("Hello Arduino"));
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+
+            byte[] response = new byte[2];
+            //arduino.Read(response); // this funtion will read data from Arduino 
+            Messages.Text += response[0].ToString();
         }
     }
 }
